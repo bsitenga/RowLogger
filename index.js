4d85cecbea18a84dd86afda2c6dc5bced96d7a0b
bsitenga <sitenga@usc.edu>
2020-03-28 12:08:16 -0700

    added react app to heroku

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ const app = express();
 app.use(cors());
 const port = process.env.PORT || 5000;
 
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
+
 //connect to the database
 mongoose.connect(process.env.DB, { useNewUrlParser: true })
   .then(() => console.log(`Database connected successfully`))
